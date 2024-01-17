77bf8fffac8ef82714fe07fc31ca35f0cdbf520b
Orennji Lu <orangeshirt23@gmail.com>
2024-01-17 09:28:40 +0900

    cssの修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Josefin_Sans } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Josefin_Sans({ 
+  weight: ['300', '600'],
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
