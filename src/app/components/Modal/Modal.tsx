0daa826161659b4cd97686481e4aab83521f066f
Orennji Lu <orangeshirt23@gmail.com>
2024-02-17 10:34:07 +0900

    to the onSubmit.

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -6,34 +6,37 @@ type ModalProps = {
   close: () => void;
   title: string;
   children: any;
-  onClick: any;
+  onClick?: any;
+  onSubmit: any;
 }
 
-export default function Modal({ show, close, title, children, onClick }: ModalProps) {
+export default function Modal({ show, close, title, children, onSubmit }: ModalProps) {
 
-  return <>
-        <div className={`${style.modalContainer} ${show ? style.show : ""}`} onClick={() => close()}>
-          <div className={style.modal} onClick={(e) => e.stopPropagation()}>
-            <header className={style.modalHeader}>
-              <h2 className={style.modalTitle}>
-                {title}
-              </h2>
-              <button className={style.close}>
-                <img className={style.icon} src={CloseIcon.src} alt="close" onClick={() => close()}/>
-              </button>
-            </header>
-            <main className={style.modalContent}>
-              {children}
-            </main>
-            <footer className={style.modalFooter}>
-              <button className="modal-close" onClick={() => close()}>
-                Cancel
-              </button>
-              <button className="submit" onClick={onClick}>
-                Submit
-              </button>
-            </footer>
-          </div>
+  return (
+    <>
+      <div className={`${style.modalContainer} ${show ? style.show : ""}`} onClick={() => close()}>
+        <div className={style.modal} onClick={(e) => e.stopPropagation()}>
+          <header className={style.modalHeader}>
+            <h2 className={style.modalTitle}>
+              {title}
+            </h2>
+            <button className={style.close}>
+              <img className={style.icon} src={CloseIcon.src} alt="close" onClick={() => close()}/>
+            </button>
+          </header>
+          <main className={style.modalContent}>
+            {children}
+          </main>
+          <footer className={style.modalFooter}>
+            <button className="modal-close" onClick={() => close()}>
+              Cancel
+            </button>
+            <button className="submit" onClick={onSubmit}>
+              Submit
+            </button>
+          </footer>
         </div>
-    </>;
+      </div>
+    </>
+  )
 };
